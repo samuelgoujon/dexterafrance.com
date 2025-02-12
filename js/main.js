2651b17af5653e2f304c108aaf6d66b57c99eb12
Trait2Jauge <88188206+Trait2Jauge@users.noreply.github.com>
2025-02-12 20:15:24 +0100

    fix:margin svg

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ let chart, feeds;
 // request data using d3.csv and initilize chart when data is loaded
 d3.csv('data/dextera_orgs.csv').then(data => {
     let width = document.getElementById('svg-container').getBoundingClientRect().width;
-    let height = window.innerHeight - 50;
+    let height = window.innerHeight;
     chart = initChart(width, height, '#svg-container', data);
 })
 
